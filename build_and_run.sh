65b15638c744c11efff0f388d0afb522b86600ce
Anders Johannsen <anders@johannsen.com>
2015-06-01 22:22:52 +0200

    add explicit pruner options

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,4 +1,16 @@
 make
 make install
-GLOG_logtostderr=1 ./TurboParser --model_type=basic --prune_basic=0 --train --file_train=data/sample/sample_train.conll --file_model=models/sample_parser.model
-GLOG_logtostderr=1 ./TurboParser --model_type=basic --test --evaluate --file_test=data/sample/sample_test.conll --file_model=models/sample_parser.model --file_prediction=data/sample/sample_test.conll.predicted
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:`pwd;`/deps/local/lib:"
+mkdir -p models
+GLOG_logtostderr=1 ./TurboParser --model_type=basic \
+	--prune_basic=1 --prune_labels=1 --prune_distances=1 \
+	--train \
+	--file_train=data/sample/sample_train.conll \
+	--file_model=models/sample_parser.model
+GLOG_logtostderr=1 ./TurboParser \
+	--model_type=basic \
+	--test --evaluate \
+	--file_test=data/sample/sample_test.conll \
+	--file_model=models/sample_parser.model \
+	--file_prediction=data/sample/sample_test.conll.predicted \
+	--output_scores
